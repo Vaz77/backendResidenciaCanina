0260273f55bc83d9136c9edbe54487faecd87a5e
Jesus <jesusvazquez2477@gmail.com>
2023-07-17 15:54:52 +0200

    Añado los seeders para Role, ademas creo los archivos seeders de los demas modelos (sin actualizar)

---
+++ seeders/20230717134526-roleSeeder.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Hago un insert en bulk a la tabla que le indico en el primer argumento, en el segundo coloco ya la información como tal dentro de un array de objetos. Cada objeto es un registro dentro de la tabla.
+    await queryInterface.bulkInsert(
+      "roles",
+      [
+        {
+          // Tengo que meter TODOS los campos, incluyendo ids y timestamps.
+          id: 1,
+          name: "Admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: "Employee",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          name: "Client",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("roles", null, {});
+  }
+};
