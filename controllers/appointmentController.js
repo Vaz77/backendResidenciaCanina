bf7413e8c56df4cae338d8d111ddb50226af3bea
Jesus <jesusvazquez2477@gmail.com>
2023-07-20 08:51:10 +0200

    Implemento controlador para editar las citas

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -3,22 +3,82 @@ const { Appointment } = require("../models");
 const appointmentController = {};
 const isActive = true;
 appointmentController.createAppointment = async (req, res) => {
-    let body = req.body;
-    try {
-      const newAppointment = await Appointment.create({
-       time: body.time,
-       status: isActive,
-       date: body.date,
-       observations: body.observations,
-       dog_id: body.dogId,
-       service_id: body.service_id,
+  let body = req.body;
+  try {
+    const newAppointment = await Appointment.create({
+      time: body.time,
+      status: isActive,
+      date: body.date,
+      observations: body.observations,
+      dog_id: body.dogId,
+      service_id: body.service_id,
+    });
+    return res.json({
+      message: "Reserva creada",
+      data: newAppointment,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+appointmentController.getAllAppointments = async (req, res) => {
+  try {
+    let appointments = await Appointment.findAll({});
+    res.json({
+      message: "Appointments found successfully",
+      data: appointments,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Appointments not found",
+      error: error,
+    });
+  }
+};
+
+appointmentController.updateAppointment = async (req, res) => {
+  try {
+    // Recojo los datos del body
+    const body = req.body;
+    const appointmentId = req.params.id;
+    const appointment = await Appointment.findOne({
+      where: {
+        id: appointmentId,
+      },
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: "Appointment not found",
       });
-      return res.json({
-        message: "Reserva creada",
-        data: newAppointment,
-      });
-    } catch (error) {
-      console.error(error);
     }
-  };
-  (module.exports = appointmentController);
+    await Appointment.update(
+      {
+        time: body.time,
+        status: body.status,
+        date: body.date,
+        observations: body.observations,
+        service_id: req.params.id,
+      },
+      {
+        where: {
+          id: appointmentId,
+        },
+      }
+    );
+    res.json({
+      message: "appointment updated",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Could not update appointment",
+      error: error,
+    });
+  }
+};
+
+module.exports = appointmentController;
--- views/appointmentRoutes.js
+++ views/appointmentRoutes.js
@@ -1,7 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const appointmentController = require("../controllers/appointmentController");
 const authMiddleware = require("../middlewares/verifyToken");
 
-router.post("/",authMiddleware, appointmentController.createAppointment)
+router.post("/", authMiddleware, appointmentController.createAppointment);
+router.get("/getAllAppointments", appointmentController.getAllAppointments);
+router.put("/update/:id", appointmentController.updateAppointment);
 
-module.exports = router
+module.exports = router;
